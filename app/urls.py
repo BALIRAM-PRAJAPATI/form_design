877385125e23a32baa5e57527fc3e33c1d378508
BALIRAM-PRAJAPATI <78644382+BALIRAM-PRAJAPATI@users.noreply.github.com>
2021-11-29 16:49:44 +0530

    Update urls.py

--- app/urls.py
+++ app/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
     path('', views.user_dashboard, name='dashboard'),
     path('signup/', views.sign_up, name='signup'),
@@ -7,4 +9,4 @@ urlpatterns = [
     path('dashboard/', views.user_dashboard, name='dashboard'),
     path('logout/', views.user_logout, name='logout'),
     path('incident/', views.incident, name = 'incident_report')
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
