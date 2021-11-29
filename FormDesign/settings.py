663126243ecf2010571315d43f6322f599448550
BALIRAM-PRAJAPATI <78644382+BALIRAM-PRAJAPATI@users.noreply.github.com>
2021-11-29 16:56:04 +0530

    Update settings.py

--- FormDesign/settings.py
+++ FormDesign/settings.py
@@ -126,6 +126,5 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
